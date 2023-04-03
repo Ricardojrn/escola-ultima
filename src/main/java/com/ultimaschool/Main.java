3547bf6b2ffd1493ebaa9d92921f0a35faaddc03
Ricardojrn <cadonascimentosilva@hotmail.com>
2023-04-03 20:12:10 -0300

    adicionando testes ao projeto

--- src/main/java/com/ultimaschool/Main.java
+++ src/main/java/com/ultimaschool/Main.java
@@ -1,7 +1,31 @@
 package com.ultimaschool;
 
+import com.ultimaschool.pessoas.Estudante;
+import com.ultimaschool.pessoas.Responsavel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Estudante estudante = new Estudante("Sara Rocha");
+
+        estudante.consolidarNotas(1,6.0);
+        estudante.consolidarNotas(2,9.0);
+        estudante.consolidarNotas(3,7.0);
+        estudante.consolidarNotas(4,8.0);
+
+        System.out.println(estudante.relatorio());
+        System.out.println(estudante.verSituacao(5));
+
+
+        Responsavel responsavel = new Responsavel("Ana Rocha");
+
+        responsavel.pagaMensalidade(1,true);
+        responsavel.pagaMensalidade(2,true);
+        responsavel.pagaMensalidade(3,false);
+        responsavel.pagaMensalidade(4,true);
+        responsavel.pagaMensalidade(5,false);
+
+        System.out.println(responsavel.relatorio());
+        System.out.println(responsavel.verSituacao(1));
+        System.out.println(responsavel.verSituacao(3));
     }
 }
